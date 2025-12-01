d41226b98f12b70e2ca55b85da6e8d3da1e10d0f
Swidilator <5881366+swidilator@users.noreply.github.com>
2025-12-02 00:00:26 +0200

    Add "Wallet and DrugMarket" test

--- Game/Test/test.cpp
+++ Game/Test/test.cpp
@@ -2,19 +2,33 @@
 // Created by kyle on 2025/12/01.
 //
 
+#include <unordered_map>
 #include <catch2/catch_test_macros.hpp>
 
 import Game.Drug;
+import Game.DrugCollection;
+import Game.Wallet;
+import Game.DrugMarket;
 
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
+
+TEST_CASE("Wallet and DrugMarket", "[Wallet][DrugMarket]") {
+    auto wallet = std::make_shared<Wallet>();
+    REQUIRE(wallet->get_balance() == 0);
+
+    wallet->add_funds(1000);
+    REQUIRE(wallet->get_balance() == 1000);
+
+    DrugMarket drug_market{};
+    REQUIRE(drug_market.get_collection().check_stock("test_drug") == 0);
+    // Add test data to drug_market
+    const_cast<DrugCollection&>(drug_market.get_collection()).add_drug(std::make_unique<Drug>("test_drug"));
+    const_cast<std::unordered_map<std::string, int>&>(drug_market.get_prices())["test_drug"] = 10;
+
+    REQUIRE(drug_market.get_collection().check_stock("test_drug") == 1);
+    REQUIRE(drug_market.get_prices().at("test_drug") == 10);
+
+    auto purchase = drug_market.buy_drug("test_drug", 1, wallet);
+    REQUIRE(drug_market.get_collection().check_stock("test_drug") == 0);
+    REQUIRE(purchase.check_stock("test_drug") == 1);
+    REQUIRE(wallet->get_balance() == 990);
 }
-
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(0) == 1 );
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
-}
-
