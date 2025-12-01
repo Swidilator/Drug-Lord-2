1ce687fd04578f8d685bf6dfea3cbb49fc100d30
Swidilator <5881366+swidilator@users.noreply.github.com>
2025-12-01 23:26:03 +0200

    Add Catch2 testing setup

---
+++ Game/Test/test.cpp
@@ -0,0 +1,20 @@
+//
+// Created by kyle on 2025/12/01.
+//
+
+#include <catch2/catch_test_macros.hpp>
+
+import Game.Drug;
+
+unsigned int Factorial( unsigned int number ) {
+    return number <= 1 ? number : Factorial(number-1)*number;
+}
+
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( Factorial(0) == 1 );
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
+}
+
