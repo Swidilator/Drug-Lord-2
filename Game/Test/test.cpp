99d8fba50d7cd3c74ada43c20172e4596f590a20
Swidilator <5881366+swidilator@users.noreply.github.com>
2025-12-06 21:57:50 +0200

    Update tests.

--- Game/Test/test.cpp
+++ Game/Test/test.cpp
@@ -5,40 +5,43 @@
 #include <unordered_map>
 #include <catch2/catch_test_macros.hpp>
 
-import Game.Drug;
-import Game.DrugCollection;
+import Game.Item;
+import Game.ItemCollection;
+import Game.ItemMarket;
 import Game.Wallet;
-import Game.DrugMarket;
 
-auto make_test_drug_collection() -> DrugCollection {
-    DrugCollection dc{};
+auto make_test_drug_collection(const int& num_drugs_1, const int& num_drugs_2) -> ItemCollection {
+    ItemCollection dc{};
 
     // Add 8 test_drug_1
-    for (int i = 0; i < 8; i++) {
-        dc.add_drug(std::make_unique<Drug>("test_drug_1"));
+    for (int i = 0; i < num_drugs_1; i++) {
+        dc.add_item(Item("test_drug_1", ItemType::Drug));
     }
 
     // Add 200 test_drug_2
-    for (int i = 0; i < 200; i++) {
-        dc.add_drug(std::make_unique<Drug>("test_drug_2"));
+    for (int i = 0; i < num_drugs_2; i++) {
+        dc.add_item(Item("test_drug_2", ItemType::Drug));
     }
     return std::move(dc);
 }
 
-TEST_CASE("Test make_test_drug_collection()", "[Drug][DrugCollection]") {
-    auto dc = make_test_drug_collection();
-    REQUIRE(dc.check_stock("test_drug_1") == 8);
-    REQUIRE(dc.check_stock("test_drug_2") == 200);
+TEST_CASE("Test make_test_drug_collection()", "[Item][ItemCollection]") {
+    constexpr int num_drugs_1 = 8;
+    constexpr int num_drugs_2 = 200;
 
-    dc.add_drug(std::make_unique<Drug>("test_drug_1"));
-    dc.add_drug(std::make_unique<Drug>("test_drug_2"));
-    REQUIRE(dc.check_stock("test_drug_1") == 9);
-    REQUIRE(dc.check_stock("test_drug_2") == 201);
+    auto dc = make_test_drug_collection(num_drugs_1, num_drugs_2);
+    REQUIRE(dc.check_stock("test_drug_1") == num_drugs_1);
+    REQUIRE(dc.check_stock("test_drug_2") == num_drugs_2);
 
-    dc.remove_drug("test_drug_1");
-    dc.remove_drug("test_drug_2");
-    REQUIRE(dc.check_stock("test_drug_1") == 8);
-    REQUIRE(dc.check_stock("test_drug_2") == 200);
+    dc.add_item(Item("test_drug_1", ItemType::Drug));
+    dc.add_item(Item("test_drug_2", ItemType::Drug));
+    REQUIRE(dc.check_stock("test_drug_1") == num_drugs_1 + 1);
+    REQUIRE(dc.check_stock("test_drug_2") == num_drugs_2 + 1);
+
+    dc.remove_item("test_drug_1");
+    dc.remove_item("test_drug_2");
+    REQUIRE(dc.check_stock("test_drug_1") == num_drugs_1);
+    REQUIRE(dc.check_stock("test_drug_2") == num_drugs_2);
 }
 
 
@@ -51,7 +54,7 @@ TEST_CASE("Wallet constructors", "[Wallet]") {
 }
 
 TEST_CASE("Wallet add and remove funds", "[Wallet]") {
-    auto wallet = std::make_shared<Wallet>(1000);
+    const auto wallet = std::make_shared<Wallet>(1000);
 
     wallet->add_funds(10);
     REQUIRE(wallet->get_balance() == 1010);
@@ -65,18 +68,31 @@ TEST_CASE("Wallet add and remove funds", "[Wallet]") {
     REQUIRE(remove_result == false);
 }
 
+TEST_CASE("ItemMarket constructors", "[ItemMarket]") {
+    constexpr int num_drugs_1 = 8;
+    constexpr int num_drugs_2 = 200;
 
-TEST_CASE("Wallet and DrugMarket", "[Wallet][DrugMarket]") {
+    const ItemMarket im1{};
+    REQUIRE(im1.get_collection().check_stock("test_drug_1") == 0);
+    ItemMarket im2{make_test_drug_collection(num_drugs_1, num_drugs_2)};
+    REQUIRE(im2.get_collection().check_stock("test_drug_1") == num_drugs_1);
+}
+
+
+TEST_CASE("Wallet and DrugMarket", "[Wallet][ItemMarket]") {
+    constexpr int num_drugs_1 = 8;
+    constexpr int num_drugs_2 = 200;
+
     auto wallet = std::make_shared<Wallet>();
     REQUIRE(wallet->get_balance() == 0);
 
     wallet->add_funds(1000);
     REQUIRE(wallet->get_balance() == 1000);
 
-    DrugMarket drug_market{};
+    ItemMarket drug_market{};
     REQUIRE(drug_market.get_collection().check_stock("test_drug") == 0);
     // Add test data to drug_market
-    const_cast<DrugCollection&>(drug_market.get_collection()) = make_test_drug_collection();
+    drug_market = ItemMarket{make_test_drug_collection(num_drugs_1, num_drugs_2)};
     const_cast<std::unordered_map<std::string, int>&>(drug_market.get_prices())["test_drug_1"] = 10;
     const_cast<std::unordered_map<std::string, int>&>(drug_market.get_prices())["test_drug_2"] = 12;
 
@@ -85,7 +101,7 @@ TEST_CASE("Wallet and DrugMarket", "[Wallet][DrugMarket]") {
 
     auto purchase = drug_market.buy_drug("test_drug_1", 1, wallet);
     REQUIRE(purchase.check_success() == true);
-    auto dc = purchase.extract_drug_collection();
+    auto dc = purchase.extract_item_collection();
     REQUIRE(purchase.check_extracted() == true);
 
     REQUIRE(drug_market.get_collection().check_stock("test_drug_1") == 7);
