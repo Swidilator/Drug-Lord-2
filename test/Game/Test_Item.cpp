b4fc055da86e6d03b22e2b689eb2c4d6f9b3135b
Swidilator <5881366+swidilator@users.noreply.github.com>
2025-12-13 14:11:35 +0200

    Add Test_Item.cpp.

---
+++ test/Game/Test_Item.cpp
@@ -0,0 +1,50 @@
+//
+// Created by kyle on 2025/12/12.
+//
+
+#include <catch2/catch_test_macros.hpp>
+
+import Game.Item;
+
+// Test
+
+TEST_CASE("Item - A new Item should have the name and ItemType from arguments", "[Item]") {
+    const Item i{"test_item", ItemType::Drug};
+
+    CHECK(i.get_name() == "test_item");
+    CHECK(i.get_item_type() == ItemType::Drug);
+
+    CHECK(i.get_price_last_bought_at() == 0);
+    CHECK(i.get_price_last_sold_at() == 0);
+}
+
+TEST_CASE("Item - An Item can have it's bought and sold values changed", "[Item]") {
+    Item i{"test_item", ItemType::Drug};
+
+    CHECK(i.get_price_last_bought_at() == 0);
+    CHECK(i.get_price_last_sold_at() == 0);
+
+    i.set_price_last_bought_at(25);
+    i.set_price_last_sold_at(44);
+
+    CHECK(i.get_price_last_bought_at() == 25);
+    CHECK(i.get_price_last_sold_at() == 44);
+}
+
+TEST_CASE("Item - An Item is moved correctly", "[Item]") {
+    Item i{"test_item", ItemType::Drug};
+    i.set_price_last_bought_at(25);
+    i.set_price_last_sold_at(44);
+
+    Item i2 = std::move(i);
+
+    CHECK(i.get_name().empty() == true);
+    CHECK(i.get_item_type() == ItemType::None);
+    CHECK(i.get_price_last_bought_at() == 0);
+    CHECK(i.get_price_last_sold_at() == 0);
+
+    CHECK(i2.get_name() == "test_item");
+    CHECK(i2.get_item_type() == ItemType::Drug);
+    CHECK(i2.get_price_last_bought_at() == 25);
+    CHECK(i2.get_price_last_sold_at() == 44);
+}
