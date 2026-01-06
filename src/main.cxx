26844d7cd548124f1265174cd1f62d644ba82bc4
Swidilator <5881366+Swidilator@users.noreply.github.com>
2026-01-06 18:34:41 +0200

    Cleanup currently unused main.cxx so that it compiles

--- src/main.cxx
+++ src/main.cxx
@@ -1,15 +1,7 @@
 #include <iostream>
 #include <memory>
 
-import Game.Character;
-import Game.Drug;
-import Game.DrugCollection;
-import Game.Wallet;
-import Game.DrugMarket;
 
 int main() {
-
-
-
     return 0;
 }
