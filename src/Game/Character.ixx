6c597ca0908df4af11da5e366362bdb2f97c6da0
Swidilator <5881366+Swidilator@users.noreply.github.com>
2026-01-08 19:04:20 +0200

    Add Character class and tests

--- src/Game/Character.ixx
+++ src/Game/Character.ixx
@@ -2,23 +2,90 @@
 // Created by kyle on 2025/11/30.
 //
 module;
-#include <string>
+#include <algorithm>
+#include <format>
+#include <memory>
+#include <unordered_map>
+#include <utility>
 
 export module Game.Character;
-
+import Game.Item;
+import Game.Wallet;
+import Game.ItemCollection;
+import Game.Inventory;
+import Game.CharacterRank;
 
 export class Character {
-    long long cash_{0};
-    long long bank_{0};
-    long long debt_{0};
+    std::shared_ptr<Wallet> cash_{};
+    std::shared_ptr<Wallet> bank_{};
+
+    // TODO: Debt will be implemented later
+
+    Inventory inventory_{};
 
     int health_{100};
 
-    int day_{1};
-
-    std::string location_{"Austin, USA"};
+    CharacterRank rank_;
 
 public:
-    Character() {
+    explicit Character(CharacterRank rank)
+        : cash_{std::make_shared<Wallet>()},
+          bank_{std::make_shared<Wallet>()},
+          rank_{std::move(rank)} {
+    }
+
+    [[nodiscard]]
+    auto health() const -> int {
+        return health_;
+    }
+
+    auto set_health(const int health) -> void {
+        if (health < 0 || health > 100) {
+            throw std::out_of_range("Health can only be set to between 0 and 100");
+        }
+        health_ = health;
+    }
+
+    [[nodiscard]]
+    auto calculate_armour(const std::unordered_map<std::string, int>& armour_values) -> int {
+        int max_armour_value{};
+
+        for (
+            const auto armour_collection{inventory_.get_collection_ptr<ItemType::Armour>().lock()}
+            ; const auto& [armour_name, armour_value]
+            : armour_collection->get_stock_count()) {
+            if (armour_values.contains(armour_name)) {
+                max_armour_value = std::max(max_armour_value, armour_values.at(armour_name));
+            } else {
+                throw std::logic_error(std::format("{} not valid armour name", armour_name));
+            }
+        }
+
+        return max_armour_value;
+    }
+
+    [[nodiscard]]
+    auto cash_wallet_ptr() -> std::weak_ptr<Wallet> {
+        return cash_;
+    }
+
+    [[nodiscard]]
+    auto bank_wallet_ptr() -> std::weak_ptr<Wallet> {
+        return bank_;
+    }
+
+    [[nodiscard]]
+    auto rank() const -> const CharacterRank& {
+        return rank_;
+    }
+
+    auto set_rank(const CharacterRank& rank) -> void {
+        rank_ = rank;
+    }
+
+    template<ItemType T>
+    [[nodiscard]]
+    auto inv_collection_ptr() -> std::weak_ptr<ItemCollection<T> > {
+        return inventory_.get_collection_ptr<T>();
     }
 };
---
+++ test/Game/Test_Character.cpp
@@ -0,0 +1,91 @@
+//
+// Created by kyle on 2025/12/16.
+//
+
+#include <unordered_map>
+
+#include "test_setup.hpp"
+
+import Game.Item;
+import Game.ItemCollection;
+import Game.Character;
+import Game.CharacterRank;
+import Game.Wallet;
+
+TEST_CASE("Character: should have 100 health on creation", "[Character]") {
+    Character c{{"test_rank", 200000, 50000}};
+
+    CHECK(c.health() == 100);
+}
+
+TEST_CASE("Character: wallets can be retrieved", "[Character]") {
+    Character c{{"test_rank", 200000, 50000}};
+
+    CHECK(typeid(c.cash_wallet_ptr()) == typeid(std::weak_ptr<Wallet>));
+    CHECK(typeid(c.bank_wallet_ptr()) == typeid(std::weak_ptr<Wallet>));
+
+    CHECK(c.cash_wallet_ptr().lock() != nullptr);
+    CHECK(c.bank_wallet_ptr().lock() != nullptr);
+
+    CHECK(c.cash_wallet_ptr().lock()->get_balance() == 0);
+    CHECK(c.bank_wallet_ptr().lock()->get_balance() == 0);
+}
+
+TEST_CASE("Character: health can be set only between 0 and 100", "[Character]") {
+    Character c{{"test_rank", 200000, 50000}};
+
+    CHECK_NOTHROW(c.set_health(0));
+    CHECK(c.health() == 0);
+    CHECK_NOTHROW(c.set_health(15));
+    CHECK(c.health() == 15);
+    CHECK_NOTHROW(c.set_health(100));
+    CHECK(c.health() == 100);
+
+    CHECK_THROWS_AS(c.set_health(-1), std::out_of_range);
+    CHECK_THROWS_AS(c.set_health(101), std::out_of_range);
+}
+
+TEST_CASE("Character: Inventory ItemCollections can be accessed", "[Character]") {
+    Character c{{"test_rank", 200000, 50000}};
+
+    auto drug_collection{c.inv_collection_ptr<ItemType::Drug>().lock()};
+    CHECK(drug_collection != nullptr);
+
+    auto armour_collection{c.inv_collection_ptr<ItemType::Armour>().lock()};
+    CHECK(armour_collection != nullptr);
+}
+
+
+TEST_CASE("Character: CharacterRank can be accessed and set", "[Character]") {
+    const CharacterRank cr{"test_rank", 200000, 50000};
+    Character c{cr};
+
+    CHECK(c.rank().title() == cr.title());
+
+    const CharacterRank cr_new{"test_rank_new", 20, 50};
+    c.set_rank(cr_new);
+    CHECK(c.rank().title() == cr_new.title());
+}
+
+TEST_CASE("Character: resultant armour value from items can be calculated", "[Character]") {
+    const std::unordered_map<std::string, int> armour_values{{"heavy leather coat", 5}, {"bullet proof vest", 10}};
+
+    Character c{{"test_rank", 200000, 50000}};
+    CHECK(c.calculate_armour(armour_values) == 0);
+
+    auto armour_collection{c.inv_collection_ptr<ItemType::Armour>().lock()};
+    armour_collection->add_item({"heavy leather coat"});
+    CHECK(c.calculate_armour(armour_values) == 5);
+
+    armour_collection->add_item({"bullet proof vest"});
+    CHECK(c.calculate_armour(armour_values) == 10);
+
+    armour_collection->add_item({"heavy leather coat"});
+    CHECK(c.calculate_armour(armour_values) == 10);
+}
+
+TEST_CASE("Character: cannot calculate armour when holding invalid armour", "[Character]") {
+    Character c{{"test_rank", 200000, 50000}};
+    c.inv_collection_ptr<ItemType::Armour>().lock()->add_item({"unknown armour"});
+    CHECK_THROWS_AS(c.calculate_armour({{"known armour", 10}}), std::logic_error);
+}
