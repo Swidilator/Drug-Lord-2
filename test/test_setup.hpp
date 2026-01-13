5202d99d531e3f66024dc7bb6351fdd43808ae39
Swidilator <5881366+Swidilator@users.noreply.github.com>
2026-01-13 19:25:39 +0200

    Testing changes to include CTest support and remove test.cpp

--- test/test_setup.hpp
+++ test/test_setup.hpp
@@ -5,3 +5,5 @@
 #pragma once
 #define CATCH_CONFIG_CONSOLE_WIDTH 800
 #include <catch2/catch_test_macros.hpp>
+
+#define TESTING_YOUTUBE_VIDEO "https://youtu.be/MwoAM3sznS0?si=PMcZnm5_vrE7lQ-e&t=2622";
