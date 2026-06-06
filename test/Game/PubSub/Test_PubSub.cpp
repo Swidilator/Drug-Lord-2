273fd0ea458147dc4d4cc0cadf74fb499577943c
Swidilator <5881366+swidilator@users.noreply.github.com>
2026-06-06 21:22:15 +0200

    Update Test_PubSub.cpp with changes to PubSub classes and extra tests

--- test/Game/PubSub/Test_PubSub.cpp
+++ test/Game/PubSub/Test_PubSub.cpp
@@ -29,8 +29,10 @@ TEST_CASE("Publisher: can be reset, clearing its subscribers and queue", "[Publi
     Publisher::reset();
     auto test_subscriber = std::make_shared<TestSubscriber>();
     Publisher::subscribe("test_topic", test_subscriber);
+    Publisher::publish("test_topic");
     Publisher::reset();
     CHECK(Publisher::list_subscribers().size() == 0);
+    CHECK(Publisher::process_next()[0] == PublisherProcessResult::Empty);
 }
 
 TEST_CASE("Subscriber: can be subscribed to a particular topic", "[Subscriber]") {
@@ -44,8 +46,8 @@ TEST_CASE("Publisher: is static and does not need to be instantiated", "[Publish
     Publisher::reset();
     auto test_subscriber = std::make_shared<TestSubscriber>();
     Publisher::subscribe("test_topic", test_subscriber);
-    Publisher::publish({"test_topic", {}});
-    Publisher::action_all();
+    Publisher::publish("test_topic");
+    Publisher::process_all();
     CHECK(test_subscriber->event_messages.size() == 1);
 }
 
@@ -53,9 +55,9 @@ TEST_CASE("Subscriber: handles published events it subscribes to", "[Subscriber]
     Publisher::reset();
     auto test_subscriber = std::make_shared<TestSubscriber>();
     Publisher::subscribe("test_topic", test_subscriber);
-    Publisher::publish({"test_topic", {}});
-    Publisher::publish({"other_topic", {}});
-    Publisher::action_all();
+    Publisher::publish("test_topic");
+    Publisher::publish("other_topic");
+    Publisher::process_all();
     REQUIRE(test_subscriber->event_messages.size() == 1);
     CHECK(test_subscriber->event_messages[0].topic == "test_topic");
 }
@@ -65,4 +67,35 @@ TEST_CASE("Subscriber: is only registered once for a particular topic", "[Subscr
     auto test_subscriber = std::make_shared<TestSubscriber>();
     Publisher::subscribe("test_topic", test_subscriber);
     CHECK_THROWS_AS(Publisher::subscribe("test_topic", test_subscriber), std::logic_error);
+}
+
+TEST_CASE("Publisher: returns false only when encountering quit topic", "[Publisher]") {
+    Publisher::reset();
+    Publisher::publish("not_quit");
+    Publisher::publish("quit");
+    CHECK(Publisher::process_next()[0] != PublisherProcessResult::Quit);
+    CHECK(Publisher::process_next()[0] == PublisherProcessResult::Quit);
+}
+
+TEST_CASE("Publisher: returns PublisherProcessResult::InvalidSubscriber when weak_ptr is invalid on action", "[Publisher]") {
+    Publisher::reset();
+    // Subscribe temporary object that will be invalid after this line
+    Publisher::subscribe("test_topic", std::make_shared<TestSubscriber>());
+    Publisher::publish("test_topic");
+    CHECK(Publisher::process_next()[0] == PublisherProcessResult::InvalidSubscriber);
+}
+
+TEST_CASE("Subscriber: can be unsubscribed from selected topic", "[Subscriber]") {
+    Publisher::reset();
+    auto test_subscriber = std::make_shared<TestSubscriber>();
+    Publisher::subscribe("test_topic",test_subscriber);
+    Publisher::subscribe("test_topic_2",test_subscriber);
+    REQUIRE(Publisher::unsubscribe("test_topic", test_subscriber) == true);
+    Publisher::publish("test_topic");
+    Publisher::publish("test_topic_2");
+    Publisher::process_all();
+    REQUIRE(test_subscriber->event_messages.size() == 1);
+    CHECK(test_subscriber->event_messages[0].topic == "test_topic_2");
+
+    CHECK(Publisher::unsubscribe("test_topic", test_subscriber) == false);
 }
